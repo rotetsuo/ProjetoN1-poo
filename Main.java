0865a3de1811aa7821f81b2ede3caa8d2ad77b1e
rotetsuo <rotetsuo32@gmail.com>
2021-05-11 08:31:09 -0400

    Sumindo com .class

--- Main.java
+++ Main.java
@@ -8,20 +8,20 @@ public class Main {
 		Chatbot bot = new Chatbot ();
 		// criando as respostas
 		Resposta rs1 = new RespostaSimples("nome", " Meus amigos me chamam de AnhembiBot .");
-    Resposta rs2 = new RespostaSimples("musica", " Minha musica favorita é alguma do rhcp .");
-    Resposta rs3 = new RespostaSimples("jogo", " Meu jogo favorito é minecraft .");
-    ArrayList<String> piadas = new ArrayList();
+    	Resposta rs2 = new RespostaSimples("musica", " Minha musica favorita é alguma do rhcp .");
+		Resposta rs3 = new RespostaSimples("jogo", " Meu jogo favorito é minecraft .");
+		ArrayList<String> piadas = new ArrayList();
 		piadas.add(" Qual é o peixe baterista ? A truta !");
 		piadas.add(" Qual é o peixe que cai do céu? Atum !");
 		Resposta rs5 = new RespostaAleatoria ("piada", piadas );
-  Resposta rs4 = new RespostaHora("dia");
+  		Resposta rs4 = new RespostaHora("dia");
     
     
 		bot.adiciona(rs1);
-    bot.adiciona(rs2);
-    bot.adiciona(rs3);
-    bot.adiciona(rs4);
-    System.out.println(bot.processar("dia"));
+		bot.adiciona(rs2);
+		bot.adiciona(rs3);
+		bot.adiciona(rs4);
+		System.out.println(bot.processar("dia"));
 	
 	}
 }
